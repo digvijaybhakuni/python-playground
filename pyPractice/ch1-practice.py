480395befa44b7337aaa5c01f9b014c825b61527
Digvijay Bhakuni <digvijay.bhakuni@rediffmail.com>
2017-07-06 00:21:04 +0530

    more fun

--- pyPractice/ch1-practice.py
+++ pyPractice/ch1-practice.py
@@ -73,3 +73,66 @@ def reverse(text):
     return str
 
 print reverse("Diggu")
+
+
+def anti_vowel(text):
+    new_text = ""
+    for x in text:
+        if x not in "aeiouAEIOU":
+            new_text += x     
+    return new_text
+    
+
+print anti_vowel("Hey You!")
+
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
+         
+         
+def scrabble_score(word):
+    score_i = 0
+    for x in word:
+        score_i += score[x.lower()]
+    return score_i
+    
+print "Digvijay = ", scrabble_score("Digvijay")
+
+
+
+def censor(text, word):
+    lst_text = []
+    words = text.split(" ")
+    for x in words:
+        if x == word:
+            lst_text.append("*" * len(x))
+        else:
+            lst_text.append(x)
+    return " ".join(lst_text)
+    
+print censor("this hack is wack hack", "hack") 
+
+
+
+def count(sequence, item):
+    count = 0
+    for n in sequence:
+        if n == item:
+            count += 1
+    return count
+    
+    
+print "TES > ", count([1,2,1,1], 1)
+
+def purify(nums):
+    return [n for n in nums if n % 2 == 0] 
+    
+print "TEst >> ", purify([1, 2, 3, 4, 5])
+
+
+def product(nums):
+    return reduce((lambda x, y: x * y), nums)
+
+print product([4, 5, 5])
