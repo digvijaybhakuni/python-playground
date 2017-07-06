1ef9d09710b4e89bdf2dbb644d78cf004cc35edb
Digvijay Bhakuni <digvijay.bhakuni@rediffmail.com>
2017-07-06 11:38:00 +0530

    exam stats

--- pyPractice/ch1-practice.py
+++ pyPractice/ch1-practice.py
@@ -136,3 +136,44 @@ def product(nums):
     return reduce((lambda x, y: x * y), nums)
 
 print product([4, 5, 5])
+
+
+def remove_duplicates(lst):
+    new_lst = []
+    for i in lst:
+        if i not in new_lst:
+            new_lst.append(i)
+    return new_lst
+    
+
+print remove_duplicates([1,1,2,2]) 
+
+
+def median(lst):
+    lst = sorted(lst)
+    print lst
+    m = len(lst)/2
+    if len(lst) % 2 == 0:
+        print m
+        print (lst[m - 1] + lst[m])
+        return float(lst[m] + lst[m - 1]) / 2.0
+    else:
+        return lst[m]       
+    
+print median([4, 5, 5, 4])
+
+
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def grades_sum(scores):
+    if len(scores) > 0:
+        return reduce((lambda a, b: a + b), scores)
+    else:
+        return 0
+        
+
+def grades_average(scores):
+    total = float(grades_sum(scores))
+    return total / len(scores) if total > 0 else 0 
+    
+print grades_average(grades)
