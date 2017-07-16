c0e46b4778799cb11d70050a1bf5532429df3999
Digvijay Bhakuni <digvijay.bhakuni@rediffmail.com>
2017-07-16 23:04:59 +0530

    Flask App Mysql and Requirement

--- flask-app/README.md
+++ flask-app/README.md
@@ -2,3 +2,55 @@
 
 This is my flask app.
 
+https://github.com/bradtraversy/myflaskapp
+
+## Templating
+
+http://flask.pocoo.org/docs/0.12/templating/
+
+http://jinja.pocoo.org/docs/2.9/templates/
+
+## Database Script
+
+Lorem
+
+### Create Database
+
+```sql
+CREATE DATABASE `FlaskBookStore`;
+```
+
+### User Table creation log
+
+```sql
+/* 18:44:30 127.0.0.1 FlaskBookStore */ CREATE TABLE `user` (id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT);
+
+/* 18:46:50 127.0.0.1 FlaskBookStore */ ALTER TABLE `user` ADD `username` VARCHAR(50)  NOT NULL  DEFAULT ''  AFTER `id`;
+
+/* 18:47:29 127.0.0.1 FlaskBookStore */ ALTER TABLE `user` ADD `email` VARCHAR(50)  NOT NULL  DEFAULT ''  AFTER `username`;
+
+/* 18:48:10 127.0.0.1 FlaskBookStore */ ALTER TABLE `user` ADD `password` VARCHAR(100)  NOT NULL  DEFAULT ''  AFTER `email`;
+
+/* 18:48:37 127.0.0.1 FlaskBookStore */ ALTER TABLE `user` ADD `creation_date` DATE  NOT NULL  AFTER `password`;
+
+/* 18:49:56 127.0.0.1 FlaskBookStore */ ALTER TABLE `user` ADD UNIQUE INDEX `username_unique` (`username`);
+
+
+/* 18:50:40 127.0.0.1 FlaskBookStore */ ALTER TABLE `user` ADD UNIQUE INDEX `email_unique` (`email`);
+
+```
+
+
+### POSTS table create log 
+
+```sql
+/* 20:38:36 127.0.0.1 FlaskBookStore */ CREATE TABLE `posts` (id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT);
+
+/* 20:39:51 127.0.0.1 FlaskBookStore */ ALTER TABLE `posts` ADD `title` VARCHAR(50)  NOT NULL  DEFAULT ''  AFTER `id`;
+
+/* 20:40:12 127.0.0.1 FlaskBookStore */ ALTER TABLE `posts` ADD `body` TEXT  NULL  AFTER `title`;
+
+/* 20:41:03 127.0.0.1 FlaskBookStore */ ALTER TABLE `posts` ADD `author` VARCHAR(20)  NOT NULL  DEFAULT ''  AFTER `body`;
+
+/* 20:41:29 127.0.0.1 FlaskBookStore */ ALTER TABLE `posts` ADD `create_date` DATE  NOT NULL  AFTER `author`;
+```
--- flask-app/src/app.py
+++ flask-app/src/app.py
@@ -1,23 +1,121 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template_string, request, sessions, redirect, url_for, logging, flash
+from data import posts
+from flask_mysqldb import MySQL
+from passlib.hash import sha256_crypt
+from datetime import date
+from forms import RegisterForm
+from functools import wraps
 
 app = Flask(__name__)
 
 app.debug = True
 
+postsCollection = posts()
+
+# MYSQL Connection Properties 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'temp'
+app.config['MYSQL_PASSWORD'] = 'temp'
+app.config['MYSQL_DB'] = 'flaskBookStore'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor' # by default it is tuple
+
+# Init MySQL
+mysql = MySQL(app)
+
+# Filter
+# Filter filter_trimed
+def filter_trimed(s, count=100):
+    return s[:count]
+app.jinja_env.filters['trimed'] = filter_trimed
+
+@app.template_filter('uppar')
+def filter_uppar(s):
+    return s.upper()
+
+@app.template_filter('date_format')
+def date_format(d):
+    if type(d) is date:
+        return d.strftime("%A %d. %B %Y")
+    return d
+
+# App Routes
+# Index
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# /blog
+@app.route("/blog")
+def blog():
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM posts ")
+    postsDB = cur.fetchall()
+    return render_template("blog.html", posts=postsDB)
+
+# /blog/post/{id}
+@app.route("/blog/post/<string:id>")
+def post(id):
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM posts  WHERE id = %s", [id])
+    postsDB = cur.fetchone()
+    return render_template("post.html", post=postsDB)
+
+# /about
+@app.route("/about")
+def about():
+    return render_template("index.html")
+
+# /about
+@app.route("/contact")
+def contact():
+    return render_template("index.html")
+
+# /register
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    reg_form = RegisterForm(request.form)
+    if request.method == 'POST' and reg_form.validate():
+        name = reg_form.name.data
+        email = reg_form.email.data
+        username = reg_form.username.data
+        password = sha256_crypt.encrypt(str(reg_form.password.data))
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        # Commit to DB
+        mysql.connection.commit()
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=reg_form)
+
+# /login
+@app.route("/login")
+def login():
+    return render_template("index.html")
+
+
+# Tests routes
 @app.route("/sample")
 def sample_data():
     return render_template("sample-data.html", sample_count = range(20))
-
-
 
 @app.route("/hello")
 def hello():
     return "Hello Flask!"
 
+@app.route("/rendered")
+def render_html():
+    return render_template_string("<h1>Rendered HTML {{name}}</h1>", name="Digivjay")
+
+@app.route("/idea")
+def idea():
+    return render_template("sample-idea.html")
 
 
 if __name__ == "__main__":
