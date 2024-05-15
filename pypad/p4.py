dafbdc30e90b009d4aa0323e54fc00fc69ce8f36
Digvijay Bhakuni <digvijay.bhakuni@tetruscorp.com>
2024-05-15 18:32:54 +0530

    P4 python Dunder Magic

--- pypad/p4.py
+++ pypad/p4.py
@@ -96,3 +96,85 @@ print(fib10)
 
 fib20 = [fib_number for fib_number in FibonacciIterator(stop=20)]
 print(fib20)
+
+
+
+class PositiveNumber:
+    def __set_name__(self, owner, name):
+        print("set name")
+        self._name = name
+
+    def __get__(self, instance, owner):
+        print("__get__")
+        return instance.__dict__[self._name]
+
+    def __set__(self, instance, value):
+        print("__set__")
+        if not isinstance(value, int | float) or value <= 0:
+            raise ValueError("positive number expected")
+        instance.__dict__[self._name] = value
+
+
+pn = PositiveNumber()
+
+
+
+import math
+
+class Circle:
+
+
+    def __new__(cls, radius): # constructor
+        return super().__new__(cls)
+
+    def __del__(self):
+        print("del Circle")
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def __setattr__(self, name, value):
+        if name == "radius":
+            if not isinstance(value, int | float):
+                raise TypeError("radius must be a number")
+            if value <= 0:
+                raise ValueError("radius must be positive")
+        super().__setattr__(name, value)
+
+    def __getattribute__(self, name):
+        print(f"__getattribute__ called for {name}")
+        return super().__getattribute__(name)
+
+    def __getattr__(self, name):
+        print(f"__getattr__ called for {name}")
+        if name == "diameter":
+            return self.radius * 2
+        return super().__getattr__(name)
+
+    def __delattr__(self, name):
+        print("__delattr__")
+        if name == 'radius':
+            raise AttributeError(
+                f"{type(self).__name__} doesn't support attribute deletion"
+            )
+        else:
+            super().__delattr__(name)
+        print(f"{name} deleted")
+
+print(f"{'Heading Circle':=^30}")
+
+mycircle = Circle(radius=7)
+mycircle.name = "MyCircle"
+print("Name of Circle: ",mycircle.name)
+print("Circle Radius: ", mycircle.radius)
+print("Circle Diameter: ", mycircle.diameter)
+
+del mycircle.name
+
+# print("Name :", mycircle.name)
+
+del mycircle
+print(mycircle)
+
+
+
